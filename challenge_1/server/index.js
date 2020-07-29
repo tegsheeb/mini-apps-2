c00a001289126e1b27dc2e1702587ab468513325
tegshee <tegshbayar@gmail.com>
2020-07-29 11:22:38 -0700

    Set up express server

---
+++ challenge_1/server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello world from expres');
+})
+
+app.listen(port, ()=> { console.log(`Listening at http://localhost:${port}`)});
